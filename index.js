03463765b5f2fd30117776fce768b3c2c21ce43e
Vagner Bezerra <116462099+vbezerra-prof@users.noreply.github.com>
2023-08-07 16:34:37 -0400

    Criação da função subtracao

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const soma = (a, b) => {
   return a + b;
 }
 
-let resultado = soma(7, 11);
-console.log(resultado);
+const subtracao = (a, b) => {
+  return a - b;
+}
+
+let res_soma = soma(7, 11);
+console.log(res_soma);
+
+let res_sub = soma(10, 15);
+console.log(res_sub);
